866e4cd293e3ca801365643b928e7fc3c59cb705
xuanzebin <xuanzebin3@jd.com>
2022-08-07 12:26:01 +0800

    feat: 完成挂载 element 的逻辑

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from '../shared'
 import { createComponentInstance, setupComponent } from './component'
 
 export function render (vnode, container) {
@@ -5,12 +6,22 @@ export function render (vnode, container) {
 }
 
 function patch (vnode, container) {
-  // 处理组件
-  processComponent(vnode, container)
+  const { type } = vnode
+  if (typeof type === 'string') {
+    // 处理元素
+    processElement(vnode, container)
+  } else if (isObject(type)) {
+    // 处理组件
+    processComponent(vnode, container)
+  }
 }
 
 function processComponent (vnode, container) {
   mountComponent(vnode, container)
+}
+
+function processElement (vnode, container) {
+  mountElement(vnode, container)
 }
 
 function mountComponent (vode, container) {
@@ -26,3 +37,27 @@ function setupRenderEffect (instance, container) {
   patch(subtree, container)
 }
 
+function mountElement (vnode, container) {
+  const { type, props, children } = vnode
+  const el: HTMLElement = document.createElement(type)
+
+  for (let key in props) {
+    const value = props[key]
+
+    el.setAttribute(key, value)
+  }
+
+  if (typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el)
+  }
+
+  container.append(el)
+}
+
+function mountChildren (vnode, container) {
+  vnode.children.forEach(v => {
+    patch(v, container)
+  })
+}
